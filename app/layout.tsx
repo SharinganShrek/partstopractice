2985852c139b25ccc22d9d4108c979885d65d5b8
Özgün Ozan Nacitarhan <72516041+onacitarhan17@users.noreply.github.com>
2026-03-28 16:05:17 +0100

    adds analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import './globals.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { LanguageProvider } from './components/LanguageContext';
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -38,6 +39,7 @@ export default function RootLayout({
           <main className="flex-1">{children}</main>
           <Footer />
         </LanguageProvider>
+        <Analytics />
       </body>
     </html>
   );
